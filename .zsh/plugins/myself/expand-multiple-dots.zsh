259359ac5fe068d6ed4ddd710604f8075a3ecf48
crd2333 <113617021+crd2333@users.noreply.github.com>
2025-08-25 19:26:18 +0800

    fix: bug of ghost complection
    Sometimes I type `cd` and autosuggestion guess `cd xxx`, then I press `enter` and find the shell execute the former but display the latter

--- .zsh/plugins/myself/expand-multiple-dots.zsh
+++ .zsh/plugins/myself/expand-multiple-dots.zsh
@@ -1,24 +1,35 @@
-# Based on http://stackoverflow.com/a/41420448/4757
+# Inspired by http://stackoverflow.com/a/41420448/4757
+# Modified to be compatible with zsh-autosuggestions plugin
 
+# Core function that expands multiple dots notation
+# Transforms patterns like "..." to "../.." and "...." to "../../.." etc.
 function expand-multiple-dots() {
     local MATCH
+    # Match three or more consecutive dots at start of line or after space
     if [[ $LBUFFER =~ '(^| )\.\.\.+' ]]; then
+        # Replace each "..." pattern with "../.." using zsh parameter expansion
         LBUFFER=$LBUFFER:fs%\.\.\.%../..%
     fi
 }
 
+# Handle Tab completion with dot expansion
+# First expands dots, then triggers normal completion
 function expand-multiple-dots-then-expand-or-complete() {
     zle expand-multiple-dots
     zle expand-or-complete
 }
 
-function expand-multiple-dots-then-accept-line() {
-    zle expand-multiple-dots
-    zle accept-line
-}
-
+# Register the functions as ZLE widgets
 zle -N expand-multiple-dots
 zle -N expand-multiple-dots-then-expand-or-complete
-zle -N expand-multiple-dots-then-accept-line
+
+# Bind Tab key to the enhanced completion function
 bindkey '^I' expand-multiple-dots-then-expand-or-complete
-bindkey '^M' expand-multiple-dots-then-accept-line
+
+# Use ZLE hook to expand dots before line execution.
+# This approach preserves the native `Enter` key behavior while still providing the functionality,
+# ensuring compatibility with plugins like zsh-autosuggestions that depend on the original accept-line widget
+function zle-line-finish() {
+    expand-multiple-dots
+}
+zle -N zle-line-finish
